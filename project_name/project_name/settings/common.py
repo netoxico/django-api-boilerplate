6a0c856602e4f933236a7c164038b7b1958b8894
Ernesto Vargas <me@netoxico.com>
2016-03-31 16:00:32 -0600

    acctualy add rest_framework to installed_apps :P

--- project_name/project_name/settings/common.py
+++ project_name/project_name/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/{{ docs_version }/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '..')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'rest_framework'
 ]
 
 MIDDLEWARE_CLASSES = [
